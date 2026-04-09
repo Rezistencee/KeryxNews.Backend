d528ef678fb5db7e6e61cf9b14f2ad5d1f7f9be5
Rezistencee <86205500+Rezistencee@users.noreply.github.com>
2026-04-09 13:07:08 +0300

    Created User model, identity user model, update identity db context.

--- KeryxNews.Infrastructure/Persistence/ApiDbContext.cs
+++ KeryxNews.Infrastructure/Persistence/ApiDbContext.cs
@@ -1,9 +1,11 @@
+using KeryxNews.Infrastructure.Persistence.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KeryxNews.Infrastructure.Persistence;
 
-public class ApiDbContext : IdentityDbContext
+public class ApiDbContext : IdentityDbContext<AppIdentityUser, IdentityRole<Guid>, Guid>
 {
     public ApiDbContext(DbContextOptions<ApiDbContext> options)
         : base(options)
